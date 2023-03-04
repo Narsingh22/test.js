9e8387fcc28d94a9cd7fd01d3e6baaa8b7670ed2
Narsingh22 <k.narasingh22@gmail.com>
2023-03-04 17:52:33 +0530

    edited HTML and JAVASCRIPT

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
-let printEl = document.getElementById("print");
-function evenNums() {
-    for (let i = 0; i <= 10; i += 2) {
-        printEl.textContent += (i) + " ";
-    }
-}
+let printEvenNums = document.getElementById("print");
 
-let inputEl1 = document.getElementById("input1");
-let inputEl2 = document.getElementById("input2");
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
 let resultEl = document.getElementById("result");
-
-function sum() {
-    let sum = inputEl1.value * inputEl2.value;
-    resultEl.textContent = " " + "=" + sum;
-}
 
 let milimeter = document.getElementById("milimeter");
 let centimeter = document.getElementById("centimeter");
 let meter = document.getElementById("meter");
-let cm = document.getElementById("cm");
-
-milimeter.addEventListener('input', function(){
-    let mm_cm = milimeter.value * 10;
-    centimeter.value = mm_cm;
-});
-
-centimeter.addEventListener('input', function(){
-    let cm_mm = centimeter.value / 10;
-    milimeter.value = cm_mm;
-});
-
-cm.addEventListener('input', function(){
-    let m_cm = cm.value * 100;
-    meter.value = m_cm;
-});
-
-meter.addEventListener('input', function(){
-    let m_cm = meter.value / 100;
-    cm.value = m_cm;
-});
+let centimeter2 = document.getElementById("centimeter2");
 
 let givenArrEl = document.getElementById("givenArr");
 givenArrEl.textContent = "[34, 438. 53. 35, 348, 25, 57]";
-let sortingArrEl = document.getElementById("sortingArr");
+let sortedArrEl = document.getElementById("sortedArr");
+
+let divisibleNum = document.getElementById("divisible-num");
+let outputEl = document.getElementById("output");
+
+function evenNums() {
+    for (let i = 0; i <= 10; i += 2) {
+        printEvenNums.textContent += (i) + " ";
+    }
+}
+
+function multiply() {
+    let sum = num1.value * num2.value;
+    resultEl.textContent = " " + "=" + sum;
+}
+
+milimeter.addEventListener('input', function(){
+    let milimeterToCentimeter = milimeter.value / 10;
+    centimeter.value = milimeterToCentimeter;
+});
+
+centimeter.addEventListener('input', function(){
+    let centimeterToMilimeter = centimeter.value * 10;
+    milimeter.value = centimeterToMilimeter;
+});
+
+centimeter2.addEventListener('input', function(){
+    let centimeterToMeter = centimeter2.value * 100;
+    meter.value = centimeterToMeter;
+});
+
+meter.addEventListener('input', function(){
+    let meterToCentimeter = meter.value / 100;
+    centimeter2.value = meterToCentimeter;
+});
 
 function sort(){
     let arr = [34, 438, 53, 35, 348, 25, 57];
     sortingArrEl.textContent = arr.sort();
-
 }
-
-let inputEl = document.getElementById("input-el");
-let output = document.getElementById("return");
 
 function divisible(){
-    if(inputEl.value / 10){
-        console.log(true);
+    if(divisibleNum.value % 10 === 0){
+        outputEl.textContent = true;
     }else{
-        console.log(false);
+        outputEl.textContent = false;
     }
 }
-inputEl.value = "";
